8e5518daa07abad87b82553b1c6baa5ac5421ef5
Leron Tonge <lerontonge@gmail.com>
2023-11-08 05:23:18 -0400

    skeleton mode

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,29 +6,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className='flex justify-between'>
       <h3>GPO</h3>
-      <ul className='flex flex-row top-0 justify-end'>
-        <li>
-          <Link href="/">
-        Home
-          </Link>
-              </li>
-              <li>
-          <Link href="/stamps">
-            Stamps
-          </Link>
-              </li>
-              <li>
-          <Link href="/tracking">
-            Tracking
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            About
-          </Link>
-        </li>
-        
-      </ul>
+     
     </nav>
   );
 };
