3e730d2a24968ce16e21779c71f19ccb360d8055
Leron Tonge <lerontonge@gmail.com>
2023-11-09 19:34:01 -0400

    test

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,8 +3,8 @@ const News = () => {
     <>
       <div className='grid md:grid-cols-3 gap-4 sm:grid-cols-1'>
         <div className='card sm:card-compact  md:card-compact bg-base-100 shadow-xl'>
-          <figure className="px-10 pt-10">
-            <img
+          <figure >
+            <img className='max-h-fit'
               src='https://i.ibb.co/28CqsL2/MGWlwGj.jpg'
               alt='News'
             />
@@ -22,16 +22,18 @@ const News = () => {
           </div>
         </div>{' '}
         <div className='card sm:card-compact  md:card-compact bg-base-100 shadow-xl'>
-          <figure className="px-10 pt-10">
-            <img
+          <figure >
+            <img className='max-h-fit'
               src='https://i.ibb.co/R2NPdvd/post-office.jpg'
               alt='News'
             />
           </figure>
           <div className='card-body'>
-            <h2 className='card-title'>Post Office Strikes...Again!</h2>
+            <h2 className='card-title'>Post Office Strikes... Again!</h2>
             <p>
-            The staff have been out since last week, after the Ministry of Works failed to correct the problems caused by a leaking roof, including the growth and build-up of mold in the building, creating a health hazard.
+              The staff have been out since last week, after the Ministry of
+              Works failed to correct the problems caused by a leaking roof,
+              including the growth and build-up...
             </p>
             <div className='card-actions justify-end'>
               <button className='btn btn-primary'>Read More</button>
