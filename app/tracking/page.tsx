e7b3573dc9ac14cec28d8ae9467dae1345ed334e
Leron Tonge <lerontonge@gmail.com>
2023-11-10 02:42:25 -0400

    omce more

--- app/tracking/page.tsx
+++ app/tracking/page.tsx
@@ -1,48 +1,27 @@
-'use client';
 
-import { useState } from 'react';
 
 export default function Tracking() {
-  const [trackingNumber, setTrackingNumber] = useState('');
-  const [trackingResult, setTrackingResult] = useState(null);
-
-  const handleTrack = () => {
-    fetch(`https://globaltracktrace.ptc.post/gtt.api/service.svc/rest/ItemTT/${trackingNumber}`)
-      .then(response => response.json())
-      .then(data => {
-        setTrackingResult(data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  };
-
   return (
     <>
-      <div className='grid place-content-center h-96'>
+      <div className= 'grid place-content-center h-96'>
         <div className='form-control max-w-xs'>
           <label className='label'>
             <span className='label-text'>Tracking Number</span>
+            
           </label>
           <input
             type='text'
             placeholder='Eg: CP000070050AG'
             className='input input-bordered w-full max-w-xs'
-            value={trackingNumber}
-            onChange={e => setTrackingNumber(e.target.value)}
           />
           <label className='flex justify-end'>
-            <button className="btn shadow-md" onClick={handleTrack}>Track</button>
+            
+            <button className="btn shadow-md ">Track</button>
           </label>
         </div>
-      </div>
-
-      {trackingResult && (
-        <div className='mt-4'>
-          <h2>Tracking Result</h2>
-          <pre>{JSON.stringify(trackingResult, null, 2)}</pre>
-        </div>
-      )}
+      </div>{' '}
     </>
   );
 }
+
+
